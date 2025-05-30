4de8cc87fa16c4ab8cb0836fe3d75f3970239de6
ForNowImHere <114445561+ForNowImHere@users.noreply.github.com>
2025-05-30 10:22:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,6 @@ app.get('/room/:roomId', (req, res) => {
   userIcon = icons[Math.floor(Math.random() * icons.length)];
   }
 
-
   // Elements
   const usersDiv = document.getElementById('users');
 
@@ -324,4 +323,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(Voice chat server listening on port ${PORT})); 
+server.listen(PORT, () => console.log(Voice chat server listening on port ${PORT}))
